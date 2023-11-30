ae81f72852a0fbca10f11f533cff9d5a1beb5e9c
sarassaura <sarahyukinonakada@gmail.com>
2023-11-30 10:45:12 -0300

    container

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,20 @@
 <template>
-    <div>
+    <div class="container mx-auto flex flex-col h-full">
         <header>
-            <nav>
-                <ul>
-                    <li><NuxtLink to="/"><UButton>Home</UButton></NuxtLink></li>
-                    <li><NuxtLink to="/about" class="font-extrabold">About</NuxtLink></li>
+            <nav class="flex w-full justify-between bg-slate-700 p-4">
+                <p>MultiPlays</p>
+                <ul class="flex gap-4">
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li><NuxtLink to="/about">About</NuxtLink></li>
                 </ul>
             </nav>
         </header>
-        <slot />
+        <main class="flex flex-grow justify-center items-center bg-slate-900">
+            <slot />
+        </main>
+        <footer class="flex w-full bg-slate-700 p-4 justify-center">
+            <p>©sarassaura, 2023. All rights reserved.</p>
+        </footer>
     </div>
 </template>
   
