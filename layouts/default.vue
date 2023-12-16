5111cca6bece1b2d23d818e4d82cf5bf7e75022c
sarassaura <sarahyukinonakada@gmail.com>
2023-12-16 18:14:57 -0300

    asda

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,32 @@
 <template>
     <div class="container mx-auto flex flex-col h-full">
         <header>
-            <nav class="flex w-full justify-between bg-slate-700 p-4">
+            <nav class="flex w-full justify-between bg-zinc-700 p-4">
                 <p>MultiPlays</p>
                 <ul class="flex gap-4">
-                    <li><NuxtLink to="/">Home</NuxtLink></li>
-                    <li><NuxtLink to="/about">About</NuxtLink></li>
+                    <li>
+                        <NuxtLink to="/">Home</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/about">About</NuxtLink>
+                    </li>
                 </ul>
             </nav>
         </header>
-        <main class="flex flex-grow justify-center items-center bg-slate-900">
+        <main class="flex flex-grow justify-center items-center bg-zinc-900">
             <slot />
         </main>
-        <footer class="flex w-full bg-slate-700 p-4 justify-center">
+        <footer class="flex w-full bg-zinc-700 p-4 justify-center">
             <p>©sarassaura, 2023. All rights reserved.</p>
         </footer>
     </div>
 </template>
   
-  <script lang="ts" setup>
+<script lang="ts" setup>
+
+</script>
   
-  </script>
   
   
-  
-  <style>
-  
-  </style>
+<style></style>
   
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
-    <h2>Home</h2>
+  <div class="flex flex-col w-full h-full align-center">
+    <h2 class="text-center text-4xl mt-8">Games</h2>
+    <div class="grid p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 place-items-center">
+      <a v-for="item in items" class="gameContainer" href="/tictactoe">
+        {{ item.game }}
+      </a>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-
+const items = [{ game: 'Dont knw' }, { game: 'boo' }, { game: 'boo' }, { game: 'boo' }, { game: 'boo' }, { game: 'boo' }, { game: 'boo' }, { game: 'boo' }, { game: 'boo' }, { game: 'boo' }, { game: 'boo' }, { game: 'boo' }, { game: 'boo' }, { game: 'boo' }, { game: 'boo' }]
 </script>
 
 
 
 <style>
-
+.gameContainer {
+  @apply border h-40 w-full flex items-center justify-center rounded;
+}
 </style>
