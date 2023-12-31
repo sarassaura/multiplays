8c09328d689bff1077a17fcd303418f8b38b2440
sarassaura <sarahyukinonakada@gmail.com>
2023-12-31 15:00:38 -0300

    centered squares

--- pages/tic-tac-toe.vue
+++ pages/tic-tac-toe.vue
@@ -1,39 +1,30 @@
 <template>
     <div class="flex flex-col w-full h-full align-center justify-center" id="container">
-        <span class="bg-slate-500 w-fit h-fit mx-auto"></span>
+        <canvas class="bg-slate-500 mx-auto"></canvas>
     </div>
 </template>
 
 <script lang="ts" setup>
 let container
-let screen
 
 onMounted(() => {
     container = document.querySelector('#container')
-    screen = container?.firstElementChild
 
-    const canvas = document.createElement('canvas')
+    const canvas = container?.firstElementChild as HTMLCanvasElement
 
     canvas.id = "tic-tac-toe"
-    canvas.width = container?.clientWidth || 0
     canvas.height = container?.clientHeight || 0
+    canvas.width = container?.clientWidth || 0
 
     const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
 
     ctx.fillStyle = "rgba(255, 0, 0, 0.2)";
-    ctx.fillRect(100, 100, 200, 200);
+    ctx.fillRect((canvas.width / 2) - 100, (canvas.height / 2) - 100, 200, 200);
     ctx.fillStyle = "rgba(0, 255, 0, 0.2)";
-    ctx.fillRect(150, 150, 200, 200);
+    ctx.fillRect((canvas.width / 2) - 100 + 50, (canvas.height / 2) - 100 + 50, 200, 200);
     ctx.fillStyle = "rgba(0, 0, 255, 0.2)";
-    ctx.fillRect(200, 50, 200, 200);
-
-
-    screen?.appendChild(canvas)
+    ctx.fillRect((canvas.width / 2) - 100 - 50, (canvas.height / 2) - 100 - 50, 200, 200);
 })
 </script>
 
-<style>
-canvas {
-    @apply flex shrink h-full w-full;
-}
-</style>
+<style></style>
