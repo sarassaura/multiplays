174df1fd8368fcbb7d72e870a98de569b3a6f493
sarassaura <sarahyukinonakada@gmail.com>
2023-12-31 17:02:55 -0300

    waaaaaaaaaa

--- pages/tic-tac-toe.vue
+++ pages/tic-tac-toe.vue
@@ -1,30 +1,67 @@
 <template>
-    <div class="flex flex-col w-full h-full align-center justify-center" id="container">
-        <canvas class="bg-slate-500 mx-auto"></canvas>
+    <div class="flex flex-col w-full h-full align-center justify-center relative" id="container">
+        <canvas class="bg-slate-500 mx-auto absolute h-full w-full"></canvas>
     </div>
 </template>
 
 <script lang="ts" setup>
-let container
+
+class GameEngine {
+    container: HTMLDivElement;
+    canvas: HTMLCanvasElement;
+    ctx: CanvasRenderingContext2D;
+    width: number;
+    height: number
+    constructor(container: HTMLDivElement, canvas: HTMLCanvasElement) {
+        this.container = container
+        this.canvas = canvas
+        this.ctx = canvas.getContext('2d')!
+        this.width = container.clientWidth
+        this.height = container.clientHeight
+    }
+
+    initialize() {
+        this.canvas.id = "tictactoe"
+        this.resize()
+    }
+
+    resize() {
+        this.canvas.height = this.container.clientHeight
+        this.canvas.width = this.container.clientWidth
+        this.width = this.canvas.width
+        this.height = this.canvas.height
+    }
+
+    draw() {
+        this.ctx.clearRect(0, 0, this.width, this.height);
+        this.createRect("rgba(255, 0, 0, 0.2)", 200, 200, 0, 0)
+        this.createRect("rgba(0, 255, 0, 0.2)", 200, 200, 50, 50)
+        this.createRect("rgba(0, 0, 255, 0.2)", 200, 200, -50, -50)
+    }
+
+    createRect(color: string, width: number, height: number, x: number, y: number) {
+        this.ctx.fillStyle = color;
+        this.ctx.fillRect(((this.width - width) / 2) + x, ((this.height - height) / 2) + y, width, height);
+    }
+}
 
 onMounted(() => {
-    container = document.querySelector('#container')
-
+    const container = document.querySelector('#container') as HTMLDivElement
     const canvas = container?.firstElementChild as HTMLCanvasElement
 
-    canvas.id = "tic-tac-toe"
-    canvas.height = container?.clientHeight || 0
-    canvas.width = container?.clientWidth || 0
+    const engine = new GameEngine(container, canvas);
 
-    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+    engine.initialize()
+    engine.draw()
 
-    ctx.fillStyle = "rgba(255, 0, 0, 0.2)";
-    ctx.fillRect((canvas.width / 2) - 100, (canvas.height / 2) - 100, 200, 200);
-    ctx.fillStyle = "rgba(0, 255, 0, 0.2)";
-    ctx.fillRect((canvas.width / 2) - 100 + 50, (canvas.height / 2) - 100 + 50, 200, 200);
-    ctx.fillStyle = "rgba(0, 0, 255, 0.2)";
-    ctx.fillRect((canvas.width / 2) - 100 - 50, (canvas.height / 2) - 100 - 50, 200, 200);
+    window.addEventListener('resize', engine.resize)
+})
+onUnmounted(() => {
+    const container = document.querySelector('#container') as HTMLDivElement
+    const canvas = container?.firstElementChild as HTMLCanvasElement
+
+    const engine = new GameEngine(container, canvas);
+
+    window.removeEventListener('resize', engine.resize)
 })
 </script>
-
-<style></style>
