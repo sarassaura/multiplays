a49be8a35b1086001ed73d3448617dc43bf0c0d7
sarassaura <sarahyukinonakada@gmail.com>
2023-12-31 23:59:52 -0300

    ahhhhhhhhhhhhhhh

--- pages/tic-tac-toe.vue
+++ pages/tic-tac-toe.vue
@@ -5,17 +5,18 @@
 </template>
 
 <script lang="ts" setup>
-let resize: () => void
-
+let container: HTMLDivElement
+let canvas: HTMLCanvasElement
+let engine: GameEngine
 class GameEngine {
     container: HTMLDivElement;
     canvas: HTMLCanvasElement;
     ctx: CanvasRenderingContext2D;
     width: number;
     height: number
-    constructor() {
-        this.container = document.querySelector('#container') as HTMLDivElement
-        this.canvas = this.container?.firstElementChild as HTMLCanvasElement
+    constructor(container: HTMLDivElement, canvas: HTMLCanvasElement) {
+        this.container = container
+        this.canvas = canvas
         this.ctx = this.canvas.getContext('2d')!
         this.width = this.container.clientWidth
         this.height = this.container.clientHeight
@@ -36,27 +37,60 @@ class GameEngine {
 
     draw() {
         this.ctx.clearRect(0, 0, this.width, this.height);
-        this.createRect("rgba(255, 0, 0, 0.2)", 200, 200, 0, 0)
-        this.createRect("rgba(0, 255, 0, 0.2)", 200, 200, 50, 50)
-        this.createRect("rgba(0, 0, 255, 0.2)", 200, 200, -50, -50)
+        this.createRect(200, 200, undefined, undefined, {})
+        this.createRect(200, 200, 50, 50, { color: "rgba(0, 255, 0, 0.2)" })
+        this.createRect(200, 200, -50, -50)
+        this.createLine(0, 0, this.width, this.height, { thick: 10 })
+        this.createLine(0, this.height, this.width, 0, {})
+        this.createCircle(100, 0, 0, { thick: 2 })
     }
 
-    createRect(color: string, width: number, height: number, x: number, y: number) {
-        this.ctx.fillStyle = color;
-        this.ctx.fillRect(((this.width - width) / 2) + x, ((this.height - height) / 2) + y, width, height);
+    createRect(width: number, height: number, x: number = 0, y: number = 0, options?: { color?: string }) {
+        let centerX = (this.width - width) / 2;
+        let centerY = (this.height - height) / 2;
+
+        this.ctx.fillStyle = options?.color || "rgb(0,0,0)"
+
+        this.ctx.fillRect(centerX + x, centerY + y, width, height);
+    }
+
+    createLine(px1: number, py1: number, px2: number, py2: number, options?: { thick?: number }) {
+        this.ctx.beginPath();
+        this.ctx.moveTo(px1, py1);
+        this.ctx.lineTo(px2, py2);
+
+        this.ctx.lineWidth = options?.thick || 5;
+
+        this.ctx.stroke();
+    }
+
+    createCircle(radius: number, x: number = 0, y: number = 0, options?: { thick?: number }) {
+        this.ctx.beginPath();
+
+        let startAngle = 0;
+        let endAngle = 2 * Math.PI;
+
+        let centerX = (this.width) / 2;
+        let centerY = (this.height) / 2;
+
+        this.ctx.lineWidth = options?.thick || 5;
+
+        this.ctx.arc(centerX + x, centerY + y, radius, startAngle, endAngle);
+        this.ctx.stroke();
     }
 }
 
 onMounted(() => {
-    const engine = new GameEngine();
+    container = document.querySelector('#container') as HTMLDivElement
+    canvas = container.firstElementChild as HTMLCanvasElement
+    engine = new GameEngine(container, canvas);
 
     engine.initialize()
     engine.draw()
 
-    resize = engine.resize
-    window.addEventListener('resize', () => resize())
+    window.addEventListener('resize', () => engine.resize())
 })
 onUnmounted(() => {
-    window.removeEventListener('resize', () => resize())
+    window.removeEventListener('resize', () => engine.resize())
 })
 </script>
