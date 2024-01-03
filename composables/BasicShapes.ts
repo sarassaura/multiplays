9d50967539bbca6e14cea2ca27723bdba82ace89
sarassaura <sarahyukinonakada@gmail.com>
2024-01-03 18:43:31 -0300

    dg

--- composables/BasicShapes.ts
+++ composables/BasicShapes.ts
@@ -78,3 +78,157 @@ export const createCircle = (
 		}
 	};
 };
+
+export const drawLine = (
+	px1: number,
+	py1: number,
+	px2: number,
+	py2: number,
+	ctx: CanvasRenderingContext2D,
+	canvasWidth: number,
+	canvasHeight: number,
+	options?: {
+		thick?: number;
+		lineCap?: 'round' | 'butt' | 'square';
+		color?: string;
+	}
+) => {
+	let centerX = canvasWidth / 2;
+	let centerY = canvasHeight / 2;
+	ctx.beginPath();
+	ctx.moveTo(px1 + centerX, py1 + centerY);
+	ctx.lineTo(px2 + centerX, py2 + centerY);
+
+	ctx.lineWidth = options?.thick || 5;
+	ctx.strokeStyle = options?.color || 'rgb(0,0,0)';
+	ctx.lineCap = options?.lineCap || 'butt';
+
+	ctx.stroke();
+	ctx.closePath();
+};
+
+export const drawRect = (
+	width: number,
+	height: number,
+	ctx: CanvasRenderingContext2D,
+	canvasWidth: number,
+	canvasHeight: number,
+	options?: {
+		x?: number;
+		y?: number;
+		thick?: number;
+		color?: string;
+		strokeColor?: string;
+	}
+) => {
+	let centerX = (canvasWidth - width) / 2;
+	let centerY = (canvasHeight - height) / 2;
+	let x = options?.x || 0;
+	let y = options?.y || 0;
+
+	ctx.beginPath();
+
+	ctx.rect(centerX + x, centerY + y, width, height);
+
+	if (options?.color) {
+		ctx.fillStyle = options?.color || 'rgb(0,0,0)';
+		ctx.fill();
+	}
+
+	if (options?.strokeColor || (!options?.color && !options?.strokeColor)) {
+		ctx.strokeStyle = options?.strokeColor || 'rgb(255,255,255)';
+		ctx.lineWidth = options?.thick || 5;
+		ctx.rect(centerX + x, centerY + y, width, height);
+		ctx.stroke();
+	}
+
+	ctx.closePath();
+};
+
+export const drawCircle = (
+	radius: number,
+	ctx: CanvasRenderingContext2D,
+	canvasWidth: number,
+	canvasHeight: number,
+	options?: {
+		x?: number;
+		y?: number;
+		thick?: number;
+		color?: string;
+		strokeColor?: string;
+	}
+) => {
+	ctx.beginPath();
+
+	let startAngle = 0;
+	let endAngle = 2 * Math.PI;
+
+	let centerX = canvasWidth / 2;
+	let centerY = canvasHeight / 2;
+	let x = options?.x || 0;
+	let y = options?.y || 0;
+
+	ctx.arc(centerX + x, centerY + y, radius, startAngle, endAngle);
+
+	if (options?.color) {
+		ctx.fillStyle = options?.color || 'rgb(0,0,0)';
+		ctx.fill();
+	}
+
+	if (options?.strokeColor || (!options?.color && !options?.strokeColor)) {
+		ctx.lineWidth = options?.thick || 5;
+		ctx.strokeStyle = options?.strokeColor || 'rgb(0,0,0)';
+		ctx.stroke();
+	}
+
+	ctx.closePath();
+};
+
+export const renderShape = (
+	shape: Shape,
+	ctx: CanvasRenderingContext2D,
+	width: number,
+	height: number
+) => {
+	switch (shape.type) {
+		case 'Line':
+			drawLine(
+				shape.initialPoint.x,
+				shape.initialPoint.y,
+				shape.endPoint.x,
+				shape.endPoint.y,
+				ctx,
+				width,
+				height,
+				{
+					thick: shape.options?.thick,
+					lineCap: shape.options?.lineCap,
+					color: shape.options?.color
+				}
+			);
+			break;
+
+		case 'Rect':
+			drawRect(shape.width, shape.height, ctx, width, height, {
+				x: shape.centerPoint?.x,
+				y: shape.centerPoint?.y,
+				color: shape.options?.color,
+				strokeColor: shape.options?.strokeColor,
+				thick: shape.options?.thick
+			});
+			break;
+
+		case 'Circle':
+			drawCircle(shape.radius, ctx, width, height, {
+				x: shape.centerPoint?.x,
+				y: shape.centerPoint?.y,
+				color: shape.options?.color,
+				strokeColor: shape.options?.strokeColor,
+				thick: shape.options?.thick
+			});
+			break;
+
+		default:
+			console.log('Invalid shape');
+	}
+};
--- composables/GameEngine.ts
+++ composables/GameEngine.ts
@@ -1,3 +1,5 @@
+import { renderShape } from './BasicShapes';
+
 export default class GameEngine {
 	container: HTMLDivElement;
 	canvas: HTMLCanvasElement;
@@ -37,150 +39,11 @@ export default class GameEngine {
 	render() {
 		this.ctx.clearRect(0, 0, this.width, this.height);
 		this.c.clearRect(0, 0, this.width, this.height);
-		this.shapes.forEach((shape) => this.renderShape(shape, this.ctx));
-		Object.values(this.boxes).forEach((shape) =>
-			this.renderShape(shape, this.c)
+		this.shapes.forEach((shape) =>
+			renderShape(shape, this.ctx, this.width, this.height)
 		);
-	}
-
-	renderShape(shape: Shape, ctx: CanvasRenderingContext2D) {
-		switch (shape.type) {
-			case 'Line':
-				this.drawLine(
-					shape.initialPoint.x,
-					shape.initialPoint.y,
-					shape.endPoint.x,
-					shape.endPoint.y,
-					ctx,
-					{
-						thick: shape.options?.thick,
-						lineCap: shape.options?.lineCap,
-						color: shape.options?.color
-					}
-				);
-				break;
-
-			case 'Rect':
-				this.drawRect(shape.width, shape.height, ctx, {
-					x: shape.centerPoint?.x,
-					y: shape.centerPoint?.y,
-					color: shape.options?.color,
-					strokeColor: shape.options?.strokeColor,
-					thick: shape.options?.thick
-				});
-				break;
-
-			case 'Circle':
-				this.drawCircle(shape.radius, ctx, {
-					x: shape.centerPoint?.x,
-					y: shape.centerPoint?.y,
-					color: shape.options?.color,
-					strokeColor: shape.options?.strokeColor,
-					thick: shape.options?.thick
-				});
-				break;
-
-			default:
-				console.log('Invalid shape');
-		}
-	}
-
-	drawLine(
-		px1: number,
-		py1: number,
-		px2: number,
-		py2: number,
-		ctx: CanvasRenderingContext2D,
-		options?: {
-			thick?: number;
-			lineCap?: 'round' | 'butt' | 'square';
-			color?: string;
-		}
-	) {
-		let centerX = this.width / 2;
-		let centerY = this.height / 2;
-		ctx.beginPath();
-		ctx.moveTo(px1 + centerX, py1 + centerY);
-		ctx.lineTo(px2 + centerX, py2 + centerY);
-
-		ctx.lineWidth = options?.thick || 5;
-		ctx.strokeStyle = options?.color || 'rgb(0,0,0)';
-		ctx.lineCap = options?.lineCap || 'butt';
-
-		ctx.stroke();
-		ctx.closePath();
-	}
-
-	drawRect(
-		width: number,
-		height: number,
-		ctx: CanvasRenderingContext2D,
-		options?: {
-			x?: number;
-			y?: number;
-			thick?: number;
-			color?: string;
-			strokeColor?: string;
-		}
-	) {
-		let centerX = (this.width - width) / 2;
-		let centerY = (this.height - height) / 2;
-		let x = options?.x || 0;
-		let y = options?.y || 0;
-
-		ctx.beginPath();
-
-		ctx.rect(centerX + x, centerY + y, width, height);
-
-		if (options?.color) {
-			ctx.fillStyle = options?.color || 'rgb(0,0,0)';
-			ctx.fill();
-		}
-
-		if (options?.strokeColor || (!options?.color && !options?.strokeColor)) {
-			ctx.strokeStyle = options?.strokeColor || 'rgb(255,255,255)';
-			ctx.lineWidth = options?.thick || 5;
-			ctx.rect(centerX + x, centerY + y, width, height);
-			ctx.stroke();
-		}
-
-		ctx.closePath();
-	}
-
-	drawCircle(
-		radius: number,
-		ctx: CanvasRenderingContext2D,
-		options?: {
-			x?: number;
-			y?: number;
-			thick?: number;
-			color?: string;
-			strokeColor?: string;
-		}
-	) {
-		ctx.beginPath();
-
-		let startAngle = 0;
-		let endAngle = 2 * Math.PI;
-
-		let centerX = this.width / 2;
-		let centerY = this.height / 2;
-		let x = options?.x || 0;
-		let y = options?.y || 0;
-
-		ctx.arc(centerX + x, centerY + y, radius, startAngle, endAngle);
-
-		if (options?.color) {
-			ctx.fillStyle = options?.color || 'rgb(0,0,0)';
-			ctx.fill();
-		}
-
-		if (options?.strokeColor || (!options?.color && !options?.strokeColor)) {
-			ctx.lineWidth = options?.thick || 5;
-			ctx.strokeStyle = options?.strokeColor || 'rgb(0,0,0)';
-			ctx.stroke();
-		}
-
-		ctx.closePath();
+		Object.values(this.boxes).forEach((shape) =>
+			renderShape(shape, this.c, this.width, this.height)
+		);
 	}
 }
